caaf6c096f80219f4f818af3587cfc4d568f7bb8
Soniya <soniya@yahoo.com>
2026-04-29 13:45:52 +1000

    fix routing structure remove broken layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,48 +6,21 @@ import Workouts from "./pages/Workouts";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-import MainLayout from "./layouts/MainLayout";
-
 export default function App() {
   return (
     <BrowserRouter>
-
       <Routes>
 
         {/* PUBLIC ROUTES */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* PROTECTED UI ROUTES */}
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <Home />
-            </MainLayout>
-          }
-        />
-
-        <Route
-          path="/dashboard"
-          element={
-            <MainLayout>
-              <Dashboard />
-            </MainLayout>
-          }
-        />
-
-        <Route
-          path="/workouts"
-          element={
-            <MainLayout>
-              <Workouts />
-            </MainLayout>
-          }
-        />
+        {/* APP ROUTES */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/workouts" element={<Workouts />} />
 
       </Routes>
-
     </BrowserRouter>
   );
 }
